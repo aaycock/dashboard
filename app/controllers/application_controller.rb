89bfc12da26e1ec5c9026dc2723f9061152c653a
Aaron Aycock <aaron.aycock@gmail.com>
2010-08-23 19:19:33 -0400

    added standalone dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  before_filter :authorize, :except => [ :login, :forgot_password ]
+  before_filter :authorize, :except => [ :login, :forgot_password, :dashboard, :show ]
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
