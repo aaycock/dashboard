fd6dfbb364f59d1033a6e103ac858f1de6933578
Aaron Aycock <aaron.aycock@gmail.com>
2010-09-12 02:41:10 -0400

    fixed info display for todays issue

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -12,13 +12,14 @@ class DashboardController < ApplicationController
     if params[:di]
       session[:dashboard_id] = params[:di]
     end
+    time_now = Time.now+4.hours
     #user = User.find(user_id)
     #account = Account.find(user.account_id)
     @dashboard = Dashboard.find(session[:dashboard_id])
     #  @dashboard = dashboards[0] Add back when we handle multiple dashboards
     @services = Service.find_all_by_dashboard_id(session[:dashboard_id])
 
-    time = Time.now+4.hours-@offset.days
+    time = time_now-@offset.days
     @days = Array[ time, time-1.day, time-2.days, time-3.days, time-4.days ]
 
     @history_hash = Hash.new
@@ -28,7 +29,7 @@ class DashboardController < ApplicationController
 
         event = Event.find_by_sql ["select * from events where events.service_id = ? and  date(events.timestamp) = date(?) order by events.timestamp desc", service.id, day]
         if event.length > 0
-          if day.strftime("%m/%d/%Y") != Time.now.strftime("%m/%d/%Y")
+          if day.strftime("%m/%d/%Y") != time_now.strftime("%m/%d/%Y")
             event[0].level = 1
           end
           @history_hash[service.id.to_s + "-" + (day.strftime("%m/%d/%Y"))] = [ service.id, event[0].level, event[0].timestamp.strftime("%Y-%m-%d") ]
